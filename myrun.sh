77185d23d34ee92ea432f71a8be93f639aa27b76
Xu DONG <17111388+cissoidx@users.noreply.github.com>
2025-06-05 15:52:28 +0800

    Create myrun.sh
    add myrun

---
+++ myrun.sh
@@ -0,0 +1,15 @@
+docker build -t my-proj:${USER:-unknown} -f Dockerfile \
+        --build-arg USER_ID=$(id -u) \
+        --build-arg GROUP_ID=$(id -g) \
+        --build-arg USER_NAME=$USER .
+
+docker run -d --rm -it \
+        -v $PWD:/workspaces/project \
+        -v $PWD/opt:/opt \
+        -w /workspaces/project \
+        --hostname vm-${hostname} \
+        --network=host \
+        --name my-proj-dx \
+        my-proj:${USER} bash
+
+docker exec -ti  my-proj-dx /bin/bash
